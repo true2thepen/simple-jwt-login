cba597b08db8cf0591cceb58bd020426b117d471
true2thepen <steven@stevenjennings.info>
2022-12-06 06:37:08 -0500

    Update role-auth-view.php
    Code cleanup.

--- simple-jwt-login/views/role-auth-view.php
+++ simple-jwt-login/views/role-auth-view.php
@@ -4,7 +4,7 @@ use SimpleJWTLogin\Modules\Settings\AuthenticationSettings;
 use SimpleJWTLogin\Modules\Settings\SettingsErrors;
 use SimpleJWTLogin\Modules\SimpleJWTLoginSettings;
 use SimpleJWTLogin\Services\RouteService;
-use WP_Role;
+
 
 if (! defined('ABSPATH')) {
     /** @phpstan-ignore-next-line  */
@@ -15,9 +15,9 @@ if (! defined('ABSPATH')) {
  * @var SimpleJWTLoginSettings $jwtSettings
  */
 
-
+global $wp_roles;
 /** @phpstan-ignore-next-line */
-$wpRoles = get_roles();
+$wpRoles = $wp_roles;
 /** @phpstan-ignore-next-line */
 $result = count_users();
 ?>
@@ -46,30 +46,19 @@ $result = count_users();
 <div class="row">
     <div class="col-md-12">
         <h3 class="section-title">
-            <?php
-            echo isset($errorCode)
-            && $settingsErrors->generateCode(
-                SettingsErrors::PREFIX_AUTHENTICATION,
-                SettingsErrors::ERR_AUTHENTICATION_EMPTY_PAYLOAD
-            ) === $errorCode
-                ? '<span class="simple-jwt-error">!</span>'
-                : ''
-            ?>
+
             <?php echo __('Roles Allowed', 'simple-jwt-login'); ?>
         </h3>
         <div id="authentication_role_data" class="authentication_jwt_container">
         <div class="columns is-multiline is-mobile">
                 <div>
-                    
+
 						<?php
-                        // $payloadParameters = $jwtSettings->getAuthenticationSettings()->getJwtPayloadParameters();
+
                         foreach ($wpRoles->roles as $roleIndex => $role) {
-                            $numberOfLines = count($wpRoles->roles) - 1;
-                            $lineSeparator = $numberOfLines === $roleIndex
-                                ? ''
-                                : ',';
+
                             $roleName = strtolower($role['name']);
-                            
+
                             $roleCount = $result['avail_roles'][$roleName] ? $result['avail_roles'][$roleName] : 0;
                             ?>
                             <div class="column">
@@ -97,7 +86,8 @@ $result = count_users();
                                     <label class="bold" for="role_auth_<?php echo esc_attr($roleName);?>">
                                         <?php
 										echo esc_attr($roleName) . ' ';
-										// echo $role_count . ' users.';
+										echo $roleCount . ' user';
+                                        if ($roleCount !== 1){ echo 's';}
 										?>
                                     </label>
                                 </div>
